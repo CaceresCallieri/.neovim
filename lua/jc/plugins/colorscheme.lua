57375acef68ce96b0ee9cbbea24c52c7010f0781
YsmaelOutis <cacerescallieri@gmail.com>
2024-09-16 14:04:32 -0300

    Change theme for Nordic

--- lua/jc/plugins/colorscheme.lua
+++ lua/jc/plugins/colorscheme.lua
@@ -1,13 +1,10 @@
 return {
 	{
-		"tiagovla/tokyodark.nvim",
-		priority = 1000, -- Makes sure the theme is loaded first.
-		opts = {
-			-- custom options here
-		},
-		config = function(_, opts)
-			require("tokyodark").setup(opts) -- calling setup is optional
-			vim.cmd([[colorscheme tokyodark]])
+		"AlexvZyl/nordic.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nordic").load()
 		end,
 	},
 }
