bbf747f22685bfe6812bd059212863336d6e72a6
YsmaelOutis <cacerescallieri@gmail.com>
2024-09-28 22:49:07 -0300

    Add hook to allow better tmux window naming

--- init.lua
+++ init.lua
@@ -1,2 +1,13 @@
 require("jc.core")
 require("jc.lazy")
+
+-- For TMUX, allows tmux-windows-name plugin to remane tmux window when opening and closing neovim instances
+local uv = vim.loop
+
+vim.api.nvim_create_autocmd({ "VimEnter", "VimLeave" }, {
+	callback = function()
+		if vim.env.TMUX_PLUGIN_MANAGER_PATH then
+			uv.spawn(vim.env.TMUX_PLUGIN_MANAGER_PATH .. "/tmux-window-name/scripts/rename_session_windows.py", {})
+		end
+	end,
+})
