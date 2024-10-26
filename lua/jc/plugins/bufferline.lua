27128dd79d161fe74e167d95c3a513d73fd8cb84
Juan Cruz Caceres <cacerescallieri@gmail.com>
2024-10-25 22:38:33 -0300

    Bufferline: Change NvimTree tab name and icon

--- lua/jc/plugins/bufferline.lua
+++ lua/jc/plugins/bufferline.lua
@@ -6,6 +6,20 @@ return {
 		options = {
 			mode = "tabs",
 			separator_style = "slant",
+
+			name_formatter = function(buf)
+				if buf.name and buf.name:match("NvimTree") then
+					return "File Explorer"
+				end
+				return buf.name
+			end,
+
+			get_element_icon = function(element)
+				-- Check if the buffer has a name and if it's NvimTree, set a custom icon
+				if element.filetype and element.filetype:match("NvimTree") then
+					return "" -- Change this to your preferred icon
+				end
+			end,
 		},
 	},
 }
