9f8369e82189c48c87767a6f0eb0d78643d1fc76
Nomad Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-03-04 20:58:02 -0300

    Telescope: Add useful keymaps; Update function calls to follow new documentation; Add actions to close telescope window

--- lua/jc/plugins/telescope.lua
+++ lua/jc/plugins/telescope.lua
@@ -19,7 +19,10 @@ return {
 					i = {
 						["<C-k>"] = actions.move_selection_previous,
 						["<C-j>"] = actions.move_selection_next,
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+						["<C-q>"] = "close",
+					},
+					n = {
+						["q"] = "close",
 					},
 				},
 			},
@@ -42,11 +45,22 @@ return {
 
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
+		local builtin = require("telescope.builtin")
 
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+		keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+		keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+		keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+		keymap.set("n", "<leader>fy", builtin.registers, { desc = "Telescope vim registers" })
+		keymap.set("n", "<leader>fm", builtin.marks, { desc = "Telescope find marks" })
 		keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
+		keymap.set("n", "<leader>fT", builtin.help_tags, { desc = "Telescope help tags" })
+
+		keymap.set("n", "<leader>fc", "", { desc = "Telescope current..." })
+		keymap.set("n", "<leader>fcs", builtin.grep_string, { desc = "Telescope grep string word under cursor" })
+		keymap.set("n", "<leader>fcf", builtin.current_buffer_fuzzy_find, { desc = "Telescope inside current buffer" })
+
+		keymap.set("n", "<leader>fh", "", { desc = "Telescope histories..." })
+		keymap.set("n", "<leader>fhc", builtin.command_history, { desc = "Telescope command history" })
+		keymap.set("n", "<leader>fhs", builtin.search_history, { desc = "Telescope search history" })
 	end,
 }
