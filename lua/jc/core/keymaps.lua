8a93e81a4ac2d64405f308d0eb46c5c1d608da42
Nomad Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-03-04 20:53:18 -0300

    Keymaps: Add shortcut to navigate to last visited buffer; Adjust some descriptions for better accuracy

--- lua/jc/core/keymaps.lua
+++ lua/jc/core/keymaps.lua
@@ -60,8 +60,9 @@ keymap.set(
 
 -- buffer navigation
 keymap.set("n", "<leader>b", "", { desc = "Buffer navigation" })
-keymap.set("n", "<leader>bn", "<cmd>bnext<CR>", { desc = "Navigate to next buffer" })
-keymap.set("n", "<leader>bp", "<cmd>bprev<CR>", { desc = "Navigate to previous buffer" })
+keymap.set("n", "<leader>bn", "<cmd>bnext<CR>", { desc = "Navigate to next buffer in buffer list" })
+keymap.set("n", "<leader>bp", "<cmd>bprev<CR>", { desc = "Navigate to previous buffer in buffer list" })
+keymap.set("n", "<C-b>", "<C-6>", { noremap = true, silent = true, desc = "Navigate to last visited buffer" })
 
 -- Plugin general keymap, plugins keymaps are in their respective .lua file
 keymap.set("n", "<leader>p", "", { desc = "Plugins keymaps" })
