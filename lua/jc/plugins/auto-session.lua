a86b2f1d423b5ca6d63b5842f93fd6340d500fd0
Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-03-29 02:57:34 -0300

    Auto-Session: Fix old paths according to checkhealth indications

--- lua/jc/plugins/auto-session.lua
+++ lua/jc/plugins/auto-session.lua
@@ -1,15 +1,15 @@
 return {
 	"rmagatti/auto-session",
 	config = function()
-		local auto_session = require("auto-session")
+		vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions" -- Recommended to make sure filetype and highlighting work correctly after a session is restored
 
+		local auto_session = require("auto-session")
 		auto_session.setup({
-			auto_restore_enabled = false,
-			auto_session_suppress_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
+			auto_restore = false,
+			suppressed_dirs = { "~/", "~/Dev/", "~/Downloads", "~/Documents", "~/Desktop/" },
 		})
 
 		local keymap = vim.keymap
-
 		keymap.set("n", "<leader>S", "", { desc = "Session managing" })
 		keymap.set("n", "<leader>Sr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" })
 		keymap.set("n", "<leader>Ss", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" })
