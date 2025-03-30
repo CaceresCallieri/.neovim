92478a0cbebd1d90b82515be9a3e6cf44d11eded
Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-03-29 23:10:17 -0300

    Treesitter: Add required configuration property fiels

--- lua/jc/plugins/treesitter.lua
+++ lua/jc/plugins/treesitter.lua
@@ -11,15 +11,32 @@ return {
 
 		-- configure treesitter
 		treesitter.setup({ -- enable syntax highlighting
+
+			-- Not sure what this does, but it's a required property
+			modules = {},
+
+			-- Install parsers synchronously (only applied to `ensure_installed`)
+			sync_install = false,
+
+			-- Automatically install missing parsers when entering buffer
+			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+			auto_install = true,
+
+			-- List of parsers to ignore installing (or "all")
+			ignore_install = { "javascript" },
+
 			highlight = {
 				enable = true,
 			},
+
 			-- enable indentation
 			indent = { enable = true },
+
 			-- enable autotagging (w/ nvim-ts-autotag plugin)
 			autotag = {
 				enable = true,
 			},
+
 			-- ensure these language parsers are installed
 			ensure_installed = {
 				"json",
@@ -44,6 +61,8 @@ return {
 				"query",
 				"vimdoc",
 				"c",
+				"regex",
+				-- "latex",
 			},
 			incremental_selection = {
 				enable = true,
