e08dee3a33d1cafe8c3931eb193584776678e6e7
Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-04-04 18:29:52 -0300

    AI-Integration: Adjust Avante config; Disable Code Companion

--- lua/jc/plugins/obsidian.lua
+++ lua/jc/plugins/obsidian.lua
@@ -14,8 +14,6 @@ return {
 	dependencies = {
 		-- Required.
 		"nvim-lua/plenary.nvim",
-
-		-- see below for full list of optional dependencies 👇
 	},
 	opts = {
 		workspaces = {
@@ -24,7 +22,5 @@ return {
 				path = vim.fn.expand("~/Second Brain"),
 			},
 		},
-
-		-- see below for full list of options 👇
 	},
 }
