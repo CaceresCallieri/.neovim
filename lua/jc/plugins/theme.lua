c755ca1e1721b62e71579a435ce00b4188cedf30
Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-05-08 19:50:23 -0300

    Theme: Make dropdown menu darker

--- lua/jc/plugins/theme.lua
+++ lua/jc/plugins/theme.lua
@@ -53,6 +53,11 @@ return {
 						FloatBorder = { bg = "none" },
 						FloatTitle = { bg = "none" },
 
+						-- Dark completion (popup) menu
+						Pmenu = { fg = theme.ui.shade0, bg = theme.ui.bg_p1 }, -- add `blend = vim.o.pumblend` to enable transparencyel = { fg = "NONE", bg = theme.ui.bg_p2 },
+						PmenuSbar = { bg = theme.ui.bg_m1 },
+						PmenuThumb = { bg = theme.ui.bg_p2 },
+
 						-- Borderless telescope, if no background is specified, inherints transparent if true
 						TelescopeTitle = { fg = theme.ui.special, bold = true },
 						-- TelescopePromptNormal = { bg = theme.ui.bg_p1 },
@@ -62,10 +67,8 @@ return {
 						-- TelescopePreviewNormal = { bg = theme.ui.bg_dim },
 						TelescopePreviewBorder = { fg = "#C2C2C2" },
 
-						-- Save an hlgroup with dark background and dimmed foreground
-						-- so that you can use it where your still want darker windows.
 						-- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-						NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+						NormalDark = { fg = theme.ui.fg_dim, bg = "none" },
 
 						-- Popular plugins that open floats will link to NormalFloat by default;
 						-- set their background accordingly if you wish to keep them dark and borderless
