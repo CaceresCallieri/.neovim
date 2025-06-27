2a20f130c3d4d32c422d5411c5452b0aaddc1eea
Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-06-26 23:54:45 -0300

    Neovide: Fix winblend of floating windows and popups

--- lua/jc/core/options.lua
+++ lua/jc/core/options.lua
@@ -72,23 +72,20 @@ opt.conceallevel = 1 -- Required for obsidian.nvim
 if vim.g.neovide then
 	vim.o.guifont = "JetBrainsMono Nerd Font Mono:h12" -- Set font for Neovide
 
-	vim.g.neovide_normal_opacity = 0.5
+	vim.g.neovide_opacity = 0.7
 
 	-- Floating windows
 	vim.g.neovide_floating_corner_radius = 0.4
 	vim.g.neovide_floating_blur_amount_x = 2.5
 	vim.g.neovide_floating_blur_amount_y = 2.5
 
-	-- Floating window shadows
-	vim.g.neovide_floating_shadow = false
-	vim.g.neovide_floating_z_height = 0
-	vim.g.neovide_light_angle_degrees = 0
-	vim.g.neovide_light_radius = 0
+	vim.o.winblend = 90 -- Set transparency for floating windows
+	vim.o.pumblend = 90 -- Set transparency for popup menus
 
 	-- Cursor options
 	vim.g.neovide_cursor_animation_length = 0.05
 	vim.g.neovide_cursor_trail_size = 0
-	vim.g.neovide_cursor_animate_command_line = false
+	vim.g.neovide_cursor_animate_command_line = false -- FIX: Does not work
 	-- vim.g.neovide_cursor_smooth_blink = true -- TODO: Add relevant options to make this work
 
 	-- Neovide only keymaps
