19f88c7519ac9825ca3e0aed3163d485be1cfd48
Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-08-06 23:51:15 -0300

    Dashboard: Show dashboard always except when a file is passed as an nvim/neovide argument

--- lua/jc/plugins/alpha-dashboard.lua
+++ lua/jc/plugins/alpha-dashboard.lua
@@ -17,7 +17,13 @@ return {
 		-- Disable folding on alpha buffer
 		vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
 
-		-- Always run alpha, even when there is an specified directory as a command argument
-		vim.cmd("Alpha")
+		-- Only run Alpha if no file arguments were provided
+		for i = 0, vim.fn.argc() - 1 do
+			local arg = vim.fn.argv(i)
+			if vim.fn.filereadable(arg) ~= 1 then
+				vim.cmd("Alpha")
+				break
+			end
+		end
 	end,
 }
