db9d6cc425f6ca7b58c4a8657d3408fca9ae0000
Juan Cruz Caceres <cacerescallieri@gmail.com>
2025-11-08 20:57:07 -0300

    feat(lualine): display relative file paths in statusline
    Add path display to both active and inactive sections showing relative paths from current working directory (e.g., "lua/jc/plugins/lualine.lua" instead of just "lualine.lua") for better file context when working with multiple files.

--- lua/jc/plugins/lualine.lua
+++ lua/jc/plugins/lualine.lua
@@ -43,12 +43,13 @@ return {
 			sections = {
 				lualine_a = { "mode" },
 				lualine_b = { "branch" },
+				lualine_c = { { "filename", path = 1 } },
 				lualine_x = {},
 				lualine_y = { "progress" },
 				lualine_z = { "location" },
 			},
 			inactive_sections = {
-				lualine_a = { "filename" },
+				lualine_a = { { "filename", path = 1 } },
 				lualine_b = {},
 				lualine_c = {},
 				lualine_x = {},
